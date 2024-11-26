625c4118f8b46f267c96799ce8b3793eaa07b946
xieronghua <527413987@qq.com>
2024-11-26 18:43:21 +0800

    启动

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 定义微服务项目的目录
+SERVICES=(
+    "./api-gateway"
+    "./order-srv"
+    "./user-srv"
+    "./file-srv"
+    "./task-srv"
+)
+
+# 启动每个微服务
+for SERVICE in "${SERVICES[@]}"; do
+    echo "Starting service in $SERVICE..."
+    # 启动服务，并将输出重定向到日志文件
+    cd "$SERVICE" && php bin/hyperf.php start > service.log 2>&1 &
+    echo "Service started in background: $SERVICE"
+done
+
+echo "All services started."
