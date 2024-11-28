59c0423d8aaec987ca9074b7f565b056b1bc46a3
xieronghua <527413987@qq.com>
2024-11-28 16:30:45 +0800

    脚本

--- start.sh
+++ start.sh
@@ -10,9 +10,17 @@ SERVICES=(
 
 # 启动每个微服务
 for SERVICE in "${SERVICES[@]}"; do
+    echo "Updating composer in $SERVICE..."
+    # 执行 composer update
+    (cd "$SERVICE" && composer update > composer.log 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "Composer update failed in $SERVICE. Exiting."
+        exit 1
+    fi
+
     echo "Starting service in $SERVICE..."
     # 启动服务，并将输出重定向到日志文件
-    cd "$SERVICE" && php bin/hyperf.php start > service.log 2>&1 &
+    (cd "$SERVICE" && php bin/hyperf.php start > service.log 2>&1 &)
     echo "Service started in background: $SERVICE"
 done
 
